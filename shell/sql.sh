0d18371cca46c95ee8ca49e67e9642c9634928e3
zh417233956 <22386206+zh417233956@users.noreply.github.com>
2020-11-20 12:50:45 +0800

    Update sql.sh

--- shell/sql.sh
+++ shell/sql.sh
@@ -2,16 +2,16 @@
 exe_secelt(){
 HOST_NAME="192.168.4.144"
 USER_NAME="sa"
-PASS_WORD="123456"
+PASS_WORD="xxxx"
 DBNAME="Demo"
 TABLE_NAME=$1
 TABLE_NAME=${TABLE_NAME:-"User_list"}
 select_sql="select * from ${TABLE_NAME}"
 # sqlcmd -S ${HOST_NAME} -U ${USER_NAME} -P ${PASS_WORD} -d ${DBNAME} -q "${select_sql}"
-sqlcmd -U sa -P 123456 -d "Demo" -Q "DECLARE @return_value int; EXEC @return_value = [dbo].[procTest];print @return_value"
+sqlcmd -U sa -P xxxx -d "Demo" -Q "DECLARE @return_value int; EXEC @return_value = [dbo].[procTest];print @return_value"
 RETN="$?"
 echo $RETN
 }
 exe_secelt $1
 
-# sqlcmd -U sa -P 123456 -d "Demo" -1"select * from User_list"
+# sqlcmd -U sa -P xxxx -d "Demo" -1"select * from User_list"
